e342b72645f65711d236aee2229423d02cb84a12
kamoshidadesu <tatsuya2630@gmail.com>
2023-05-06 13:38:00 +0900

    条件分岐（保存されてるidによる表示の変化）

--- app/models/management.rb
+++ app/models/management.rb
@@ -4,6 +4,5 @@ belongs_to :user
 
 extend ActiveHash::Associations::ActiveRecordExtensions 
   belongs_to_active_hash :record
-
   
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,14 +1,16 @@
 class Record < ActiveHash::Base
   self.data = [
-    { id: 1, name: '___' },
-    { id: 2, name: 'ミルク', image_path: 'milk.png' },
-    { id: 3, name: 'meal', image_path: 'meal' },
-    { id: 4, name: 'poop', image_path: 'poop' },
-    { id: 5, name: 'bath', image_path: 'bath' },
-    { id: 6, name: 'hospital', image_path: 'hospital' },
-    { id: 7, name: 'injection', image_path: 'injection' },
-    { id: 8, name: 'medicine', image_path: 'medicine' },
-    { id: 9, name: 'sleep', image_path: 'sleep' }
+    { id: 1, name: '選択してください' },
+    { id: 2, name: 'ミルク', image_path: "milk.png" },
+    { id: 3, name: '食事', image_path: 'meal' },
+    { id: 4, name: 'うんち', image_path: 'poop' },
+    { id: 5, name: 'おむつ', image_path: 'diapers' },
+    { id: 6, name: 'お風呂', image_path: 'bath' },
+    { id: 7, name: '病院', image_path: 'hospital' },
+    { id: 8, name: '予防接種', image_path: 'injection' },
+    { id: 9, name: 'お薬', image_path: 'medicine' },
+    { id: 10, name: '睡眠', image_path: 'sleep' },
+    { id: 11, name: '保育園・幼稚園', image_path: 'nursery school' }
   ]
 
   include ActiveHash::Associations
