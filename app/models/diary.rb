66595e8f1fd07c955fa3d708dbc2d1b7b4bb5c2c
kamoshidadesu <tatsuya2630@gmail.com>
2023-05-25 13:26:00 +0900

    edit エラー表示調整

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -4,7 +4,8 @@ class Diary < ApplicationRecord
 
   validates :start_time, presence: true
   validates :happy, presence: true
-  validate :one_event_per_day, on: :create, on: :update
+  validate :one_event_per_day, on: :create 
+  validate :one_event_per_day_edit, on: :update 
 
   private
 
@@ -14,5 +15,13 @@ class Diary < ApplicationRecord
     elsif start_time.present? && self.class.where(start_time: start_time.beginning_of_day..start_time.end_of_day).exists?
       errors.add(:start_time, 'You have already created an event for this time.')
     end
+  end
+end
+
+def one_event_per_day_edit
+  if self.class.where(start_time: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day).where.not(id: self.id).exists?
+    errors.add(:base, 'You can only create one event per day.')
+  elsif start_time.present? && self.class.where(start_time: start_time.beginning_of_day..start_time.end_of_day).where.not(id: self.id).exists?
+    errors.add(:start_time, 'You have already created an event for this time.')
   end
 end
