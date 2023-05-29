f95c3426afaeb5391e2a4206517dbe86c03d68cc
kamoshidadesu <tatsuya2630@gmail.com>
2023-05-29 22:42:47 +0900

    calendar モデルにバリデーション追加(開始時刻よりも前の時間設定はできない）

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -4,4 +4,13 @@ class Calendar < ApplicationRecord
   validates :start_time, presence: true
   validates :end_time,   presence: true
   validates :schedule,   presence: true
+  validate  :start_end_check
+
+  private
+
+    def start_end_check
+      errors.add(:end_time, "は開始時刻より前の時間には設定できません。")
+      if self.start_time > self.end_time
+      end
+    end
 end
