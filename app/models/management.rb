29084e778c4e8e30ca223b55a352adbe16f6549a
kamoshidadesu <tatsuya2630@gmail.com>
2023-05-31 10:20:14 +0900

    management エラーメッセージのカラム名部分の日本語化

--- app/models/management.rb
+++ app/models/management.rb
@@ -3,9 +3,8 @@ class Management < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :record
-
   validates :record_id, presence: true
   
   # ジャンルの選択が「せ」の時は保存できないようにする
-  validates :record_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :record_id, numericality: { other_than: 1, message: "を選択してください" }
 end
