91e96a7562b2775e60b3a1623954f26ed402b938
kamoshidadesu <tatsuya2630@gmail.com>
2023-05-31 12:38:45 +0900

    userモデルのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # ユーザーの新規登録には、パスワードは確認用を含めて2回入力すること
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
+            confirmation: true
+# ユーザーの新規登録には、ニックネーム、メールアドレス、
+validates :name,
+            presence: true
+validates :nickname,
+          presence: true
+
   has_many :calendars
   has_many :diaries
   has_many :managements
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    name                  { Faker::Name.name}
+    nickname              { Faker::Name.name}
+    email                 { Faker::Internet.free_email}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+  end
+end
