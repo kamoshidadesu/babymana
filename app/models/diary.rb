c9e19e4eee162e7dc8e95aaeb7ce180deed32ba0
kamoshidadesu <tatsuya2630@gmail.com>
2023-05-31 10:07:20 +0900

    diary エラーメッセージのカラムの日本語化

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -11,17 +11,17 @@ class Diary < ApplicationRecord
 
   def one_event_per_day
     if self.class.where(start_time: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day).exists?
-      errors.add(:base, '一日一つしか投稿できません')
+      errors.add(:base, 'は一日一つしか書けません')
     elsif start_time.present? && self.class.where(start_time: start_time.beginning_of_day..start_time.end_of_day).exists?
-      errors.add(:start_time, '一日一つしか投稿できません')
+      errors.add(:diary, 'は一日一つしか書けません')
     end
   end
 
   def one_event_per_day_edit
     if self.class.where(start_time: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day).where.not(id: self.id).exists?
-      errors.add(:base, '一日一つしか投稿できません')
+      errors.add(:base, 'は一日一つしか書けません')
     elsif start_time.present? && self.class.where(start_time: start_time.beginning_of_day..start_time.end_of_day).where.not(id: self.id).exists?
-      errors.add(:start_time, '一日一つしか投稿できません')
+      errors.add(:diary, 'は一日一つしか書けません')
     end
   end
 end
