154a8f120e38653643f4be135f4c2656fb396338
kamoshidadesu <tatsuya2630@gmail.com>
2023-06-16 12:31:53 +0900

    preview.jsを関数ごとに切り分け整理

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,27 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
   // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('new_diary');
+  const postForm = document.getElementById('new_post');
   // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById('previews');
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
 
-  // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="diary[images][]"]');
-  // input要素で値の変化が起きた際に呼び出される関数
-  fileField.addEventListener('change', function(e){
-    // 古いプレビューが存在する場合は削除
-    const alreadyPreview = document.querySelector('.preview');
-    if (alreadyPreview) {
-      alreadyPreview.remove();
-    };
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+  // プレビュー画像を生成・表示する関数
+  const buildPreviewImage = (dataIndex, blob) =>{
     // 画像を表示するためのdiv要素を生成
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
+    previewWrapper.setAttribute('data-index', dataIndex);
+
     // 表示する画像を生成
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
@@ -30,5 +21,44 @@ document.addEventListener('DOMContentLoaded', function(){
     // 生成したHTMLの要素をブラウザに表示させる
     previewWrapper.appendChild(previewImage);
     previewList.appendChild(previewWrapper);
-  });
+  };
+
+  // file_fieldを生成・表示する関数
+  const buildNewFileField = () => {
+    // 2枚目用のfile_fieldを作成
+    const newFileField = document.createElement('input');
+    newFileField.setAttribute('type', 'file');
+    newFileField.setAttribute('name', 'diary[images][]');
+
+    // 最後のfile_fieldを取得
+    const lastFileField = document.querySelector('input[type="file"][name="diary[images][]"]:last-child');
+    // nextDataIndex = 最後のfile_fieldのdata-index + 1
+    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
+    newFileField.setAttribute('data-index', nextDataIndex);
+
+    // 生成したfile_fieldを表示
+    const fileFieldsArea = document.querySelector('.click-upload');
+    fileFieldsArea.appendChild(newFileField);
+  };
+
+   // input要素で値の変化が起きた際に呼び出される関数の中身
+    const changedFileField = (e) => {
+      // 古いプレビューが存在する場合は削除
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    buildPreviewImage(dataIndex, blob);
+    buildNewFileField();
+  };
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="post[image][]"]');
+  
+  // input要素で値の変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', changedFileField);
 });
+か
