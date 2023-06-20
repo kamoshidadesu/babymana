596b4167331114c5f9f07fe32b5447bbb3f87890
kamoshidadesu <tatsuya2630@gmail.com>
2023-06-20 23:26:21 +0900

    スライド実装完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@
 require("@rails/activestorage").start()
 require("channels")
 require('../preview')
+require('jquery')
+require('../slick')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,5 +19,6 @@ require('../preview')
 // const imagePath = (name) => images(name, true)
 
 //import Rails from 'rails-ujs'
-
+//= require jquery
+//= require rails-ujs
 
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+//ここから(slick導入に際してのwebpackの記述)
+const webpack = require('webpack')
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+//ここまでを追加
 module.exports = environment
