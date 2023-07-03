fab1f67c334701e6166967ffb67e77387756ee32
kamoshidadesu <tatsuya2630@gmail.com>
2023-07-03 17:18:50 +0900

    genderモデル作成

---
+++ app/models/gender.rb
@@ -0,0 +1,10 @@
+class Record < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '選択してください' },
+    { id: 2, name: 'ママ' },
+    { id: 3, name: 'パパ' },
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+end
