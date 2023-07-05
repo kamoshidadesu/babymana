4e6b762775e597547d858ae9b8f0c581709eeea3
kamoshidadesu <tatsuya2630@gmail.com>
2023-07-05 14:50:12 +0900

    テストコード（factorybotにgender_idを入力）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     email                 { Faker::Internet.free_email}
     password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
+    gender_id             { Faker::Number.between(from: 2, to:3) }
   end
 end
